58bcb79c5cf5729dfdac3cd36a90e675346bc6eb
Ryan Elliott-Potter <ryan@ryanep.com>
2019-07-01 07:55:15 +0100

    Type service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,18 +2,25 @@ const cacheVersion = 1;
 const cacheName = `offline-cache-${cacheVersion}`;
 const cacheFiles = ['/', '/app.css', '/app.js'];
 
-self.addEventListener('install', event => {
+const handleServiceWorkerInstall = (event: FetchEvent) => {
   event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll(cacheFiles);
     }),
   );
-});
+};
 
-self.addEventListener('fetch', event => {
+const handleServiceWorkerFetch = (event: FetchEvent) => {
   event.respondWith(
     fetch(event.request).catch(() => {
       return caches.match(event.request);
     }),
   );
-});
+};
+
+const init = (service: ServiceWorkerGlobalScope) => {
+  service.addEventListener('install', handleServiceWorkerInstall);
+  service.addEventListener('fetch', handleServiceWorkerFetch);
+};
+
+init(self as any);
